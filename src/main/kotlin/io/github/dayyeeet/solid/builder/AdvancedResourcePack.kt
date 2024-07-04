bc0be10b6d4535cc3d9abe013285bcc3c4a7e4a1
Dayeeet <david@cappell.net>
2024-07-04 15:19:48 +0200

    refactor: remove old import

--- src/main/kotlin/io/github/dayyeeet/solid/builder/AdvancedResourcePack.kt
+++ src/main/kotlin/io/github/dayyeeet/solid/builder/AdvancedResourcePack.kt
@@ -1,8 +1,7 @@
 package io.github.dayyeeet.solid.builder
 
-import de.dayeeet.solid.builder.feature.*
-import io.github.dayyeeet.solid.mappings.ModelMapper
 import io.github.dayyeeet.solid.builder.feature.*
+import io.github.dayyeeet.solid.mappings.ModelMapper
 import net.kyori.adventure.key.Key
 import team.unnamed.creative.BuiltResourcePack
 import team.unnamed.creative.ResourcePack
