c9a8a618ad6ef61ffc51997d0f42765b58b10341
Dayeeet <david@cappell.net>
2024-08-08 17:39:10 +0200

    feat: SolidMaterial by key

--- src/main/kotlin/io/github/solid/resourcepack/api/material/SolidMaterialMapper.kt
+++ src/main/kotlin/io/github/solid/resourcepack/api/material/SolidMaterialMapper.kt
@@ -3,13 +3,14 @@ package io.github.solid.resourcepack.api.material
 import io.github.solid.resourcepack.material.SolidBlockMaterial
 import io.github.solid.resourcepack.material.SolidItemMaterial
 import io.github.solid.resourcepack.material.SolidMaterial
+import net.kyori.adventure.key.Key
 import org.bukkit.Material
 import org.jetbrains.annotations.ApiStatus.Experimental
 
 object SolidMaterialMapper {
 
     /**
-     * Converts a [Material] to a [SolidMaterial]
+     * Gets a [SolidMaterial] by a [Material]
      * IMPORTANT: Only works in a server context
      */
     @Experimental
@@ -21,6 +22,17 @@ object SolidMaterialMapper {
             return SolidBlockMaterial.valueOf(material.name).toGeneric()
         }
         throw IllegalArgumentException("Material $material is not supported")
+    }
+
+    /**
+     * Gets a [SolidMaterial] by a [Key] (if this key represents a material in a resource pack context)
+     */
+    fun from(key: Key): SolidMaterial {
+        //For fastness, we use the key to determine the type, and then call from(Material, MaterialType)
+        val split = key.value().split("/")
+        val type = split.first().uppercase()
+        val material = split.last().uppercase()
+        return from(Material.valueOf(material), MaterialType.valueOf(type))
     }
 
     fun from(material: Material, type: MaterialType): SolidMaterial {
