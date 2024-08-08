46bf62a0641d769f4e62a48ccf37c6e4edb7bfcb
Dayeeet <david@cappell.net>
2024-08-08 19:36:22 +0200

    chore: update to newest solid-material

--- src/test/kotlin/PredicateTest.kt
+++ src/test/kotlin/PredicateTest.kt
@@ -1,24 +1,14 @@
 import io.github.solid.resourcepack.api.builder.AdvancedResourcePack
-import io.github.solid.resourcepack.api.builder.config.ModelVariant
 import io.github.solid.resourcepack.api.builder.config.Predicate
-import io.github.solid.resourcepack.api.key.KeyType
-import io.github.solid.resourcepack.api.key.SolidKey
 import io.github.solid.resourcepack.material.SolidMaterial
 import net.kyori.adventure.key.Key
-import org.bukkit.Material
 import team.unnamed.creative.ResourcePack
 
 val BOOST_ARROW = Key.key("test", "item/boost_arrow")
 fun main() {
     val pack = AdvancedResourcePack(ResourcePack.resourcePack())
     pack.apply(
-        Predicate.customModelData(
-            SolidMaterial(
-                SolidKey.from(Material.POPPED_CHORUS_FRUIT, KeyType.ITEM),
-                SolidKey.ITEM_GENERATED,
-                ModelVariant.Textures.generic(SolidKey.from(Material.POPPED_CHORUS_FRUIT, KeyType.ITEM))
-            )
-        ).models(BOOST_ARROW).build()
+        Predicate.customModelData(SolidMaterial.from(Key.key("minecraft", "item/stick"))).models(BOOST_ARROW).build()
     )
 
     val mapper = TestMapper()
